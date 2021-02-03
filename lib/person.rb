64abd646cbe379dbb84d3ba1d799480052230edf
Bleak Chandler <bleakchandler@gmail.com>
2021-02-03 15:58:39 -0500

    added changes (or any message you would like)

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,38 @@
 # your code goes here
-test
+
+require 'pry'
+
+class Person
+
+attr_reader :name
+attr_accessor :bank_account, :happiness, :hygine
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygine = hygine
+    end
+
+    #binding.pry
+
+    def happy?
+    end
+
+    def clean
+    end
+
+    def get_paid
+    end
+
+    def work_out
+    end
+
+    def call_friend
+    end
+
+    def start_conversation
+    end
+
+    
+end
